b6cce5587a82b16d58124d3c89e94e67b8d79115
Ryota Kobayashi <ryota5884@icloud.com>
2022-07-12 01:29:23 +0900

    maybe complete

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@
 #include "tdzdd/eval/ToZBDD.hpp"
 #include "tdzdd/spec/SapporoZdd.hpp"
 
+// #include <iostream>
 #include <vector>
+#include <list>
 #include <algorithm>
 
 using namespace tdzdd;
@@ -177,15 +179,70 @@ ZBDD getSolutionSpace(int m, int n, int K, vector<vector<int>> carInfo) {
     return selectWithLimitByIntersection(overlapped, perSquare, 1);
 }
 
-// Function to perform one transition
-// ZBDD transition(ZBDD z) {
+// Functions for transitions
+ZBDD remove(ZBDD F) {
+    if (F == ZBDD(0) || F == ZBDD(1)) {
+        return ZBDD(0);
+    } else {
+        int t = F.Top();
+        ZBDD F0 = F.OffSet(t);
+        ZBDD F1 = F.OnSet0(t);
 
-// }
+        return operator+(operator+(F1, remove(F0)), operator*(getSingleton(t), remove(F1)));
+    }
+}
+
+ZBDD add(ZBDD F, list<int> A) {
+    if (F == ZBDD(0)) {
+        return ZBDD(0);
+    } else if (F == ZBDD(1)) {
+        auto itrFirst = A.begin();
+        ZBDD z = getSingleton(*itrFirst);
+
+        for(auto itr = A.begin(); itr != A.end(); ++itr) {
+            z = operator+(z, getSingleton(*itr));
+        }
+
+        return z;
+    } else {
+        int t = F.Top();
+        ZBDD F0 = F.OffSet(t);
+        ZBDD F1 = F.OnSet0(t);
+
+        A.remove(t);
+
+        return operator+(add(F0, A), operator*(getSingleton(t), operator+(F0, add(F1, A))));
+    }
+}
+
+ZBDD swap(ZBDD F, list<int> A) {
+    if (F == ZBDD(0) || F == ZBDD(1)) {
+        return ZBDD(0);
+    } else {
+        int t = F.Top();
+        ZBDD F0 = F.OffSet(t);
+        ZBDD F1 = F.OnSet0(t);
+
+        A.remove(t);
+
+        return operator+(operator+(swap(F0, A), add(F1, A)), operator*(getSingleton(t), operator+(swap(F1, A), remove(F0))));
+    }
+}
+
+// Function to perform one transition
+ZBDD transition(ZBDD F, list<int> V, ZBDD FSol, ZBDD FPrev) {
+    return operator-(operator&(swap(F, V), FSol), FPrev);
+}
 
 int main() {
     // m, n, K denote the number of rows, columns, and cars, respectively
     int m, n, K;
     cin >> m >> n >> K;
+
+    // ans represents node number of the answer
+    int iAns, jAns, kAns;
+    cin >> iAns >> jAns >> kAns;
+    int ans = jAns + n * (iAns - 1) + m * n * (kAns - 1);
 
     // Hold the direction and length of cars
     // HACK: The method of specifying the size of a vector is (maybe) not good
@@ -197,9 +254,12 @@ int main() {
     // Memory allocation
     BDD_Init(1024, 1024 * 1024 * 1024);
 
+    // Vector of node numbers
+    list<int> V;
     // Allocate variables to be used
     for (int i = 0; i < m * n * K; i++) {
         BDD_NewVar();
+        V.push_back(i + 1);
     }
 
     // Construct initial ZDD
@@ -207,8 +267,8 @@ int main() {
     // NOTE: Rows are denoted as i, columns as j, so j is on the outside
     // NOTE: The given initial state must be a feasible solution
     // TODO: Change to detect when input is not a feasible solution
-    for (int j = 0; j < n; j++) {
-        for (int i = 0; i < m; i++) {
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
             string square;
             cin >> square;
 
@@ -217,19 +277,38 @@ int main() {
             } else {
                 // Calculate the element number from the square and car number
                 // NOTE: Node numbers begin with 1
-                initialVec.push_back((i + 1) + m * j + m * n * (stoi(square) - 1));
+                initialVec.push_back((j + 1) + n * i + m * n * (stoi(square) - 1));
             }
         }
     }
-    ZBDD zInitial = getSingleSet(initialVec);
+    ZBDD F = getSingleSet(initialVec);
 
-    ZBDD zSol = getSolutionSpace(m, n, K, carInfo);
+    // Generate solution space
+    ZBDD FSol = getSolutionSpace(m, n, K, carInfo);
 
-    // TODO: Add transition rules
-    // Transition rule is the most common
+    // count represent the number of transitions
+    int count = 0;
+    // FPred represent previous transition
+    ZBDD FPrev = ZBDD(0);
+    while (F != ZBDD(0)) {
+        ZBDD target = F.OnSet(ans);
+        if (target != ZBDD(0)) {
+            // If the target solution is reached
+            cout << count << endl;
+            cout << ZStr(target) << endl;
+            return 0;
+        } else {
+            ZBDD FTmp = F;
+            F = transition(F, V, FSol, FPrev);
+            FPrev = FTmp;
+            count++;
 
-    // FIXME: test
-    cout << ZStr(zSol) << endl;
+            // FIXME: test
+            // cout << ZStr(F) << endl;
+        }
+    }
+
+    cout << "Cannot reach the answer..." << endl;
 
     return 0;
 }
